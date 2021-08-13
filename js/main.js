a775142dad61b2859da364a0be6dfdc15eb537c3
Varamosf <varamosf@gmail.com>
2021-08-12 23:42:26 -0500

    Retomando forma para dispositivos móviles

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*Mostrar y ocultar menú de navegación*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close")
+
+/*Mostrar Menú*/
+/*Validar si las constantes existen*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu")
+  })
+}
+/*Ocultar Menú*/
+/*Validar si las constantes existen*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu")
+  })
+}
+
+const navLink = document.querySelectorAll(".nav__link")
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu")
+  /*Cuando cloqueemos en cualquier botón de navegación, removemos de la visualización la clase de mostrar menú*/
+  navMenu.classList.remove("show-menu")
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction))
